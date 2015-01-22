fc0c902f6bdb7e148e09a5537ca66f218dde4d2d
Nicolas Dupont <ndupont@ellisdon.com>
2015-01-22 14:17:05 +0100

    added project

---
+++ myproject/src/com/dupont/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.dupont;
+
+/**
+ * @author ndupont
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
